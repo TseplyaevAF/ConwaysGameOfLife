ef22a9334ee6d3105ba2f41e3f625e90c8c8c476
Alex <tseplyaeff.alex@yandex.ru>
2020-04-29 12:50:08 +0900

    add project

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox_Pause = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Clear = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Step = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Fill = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Play = new System.Windows.Forms.PictureBox();
+            this.pictureBox_GameField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Clear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Step)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Fill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Play)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_GameField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox_Pause
+            // 
+            this.pictureBox_Pause.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Pause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Pause.Image = global::GameOfLife.Properties.Resources.pause;
+            this.pictureBox_Pause.Location = new System.Drawing.Point(168, 338);
+            this.pictureBox_Pause.Name = "pictureBox_Pause";
+            this.pictureBox_Pause.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox_Pause.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Pause.TabIndex = 6;
+            this.pictureBox_Pause.TabStop = false;
+            this.pictureBox_Pause.Visible = false;
+            this.pictureBox_Pause.Click += new System.EventHandler(this.pictureBox_Pause_Click);
+            // 
+            // pictureBox_Clear
+            // 
+            this.pictureBox_Clear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Clear.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Clear.Image")));
+            this.pictureBox_Clear.Location = new System.Drawing.Point(214, 338);
+            this.pictureBox_Clear.Name = "pictureBox_Clear";
+            this.pictureBox_Clear.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox_Clear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Clear.TabIndex = 4;
+            this.pictureBox_Clear.TabStop = false;
+            this.pictureBox_Clear.Click += new System.EventHandler(this.pictureBox_Clear_Click);
+            // 
+            // pictureBox_Step
+            // 
+            this.pictureBox_Step.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Step.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Step.Image = global::GameOfLife.Properties.Resources.step;
+            this.pictureBox_Step.Location = new System.Drawing.Point(122, 338);
+            this.pictureBox_Step.Name = "pictureBox_Step";
+            this.pictureBox_Step.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox_Step.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Step.TabIndex = 3;
+            this.pictureBox_Step.TabStop = false;
+            this.pictureBox_Step.Click += new System.EventHandler(this.pictureBox_Step_Click);
+            // 
+            // pictureBox_Fill
+            // 
+            this.pictureBox_Fill.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Fill.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Fill.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Fill.Image")));
+            this.pictureBox_Fill.Location = new System.Drawing.Point(76, 338);
+            this.pictureBox_Fill.Name = "pictureBox_Fill";
+            this.pictureBox_Fill.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox_Fill.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Fill.TabIndex = 2;
+            this.pictureBox_Fill.TabStop = false;
+            this.pictureBox_Fill.Click += new System.EventHandler(this.pictureBox_Fill_Click);
+            // 
+            // pictureBox_Play
+            // 
+            this.pictureBox_Play.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox_Play.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Play.Image")));
+            this.pictureBox_Play.Location = new System.Drawing.Point(168, 338);
+            this.pictureBox_Play.Name = "pictureBox_Play";
+            this.pictureBox_Play.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox_Play.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Play.TabIndex = 1;
+            this.pictureBox_Play.TabStop = false;
+            this.pictureBox_Play.Click += new System.EventHandler(this.pictureBox_Play_Click);
+            // 
+            // pictureBox_GameField
+            // 
+            this.pictureBox_GameField.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.pictureBox_GameField.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_GameField.MaximumSize = new System.Drawing.Size(320, 320);
+            this.pictureBox_GameField.MinimumSize = new System.Drawing.Size(320, 320);
+            this.pictureBox_GameField.Name = "pictureBox_GameField";
+            this.pictureBox_GameField.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox_GameField.TabIndex = 0;
+            this.pictureBox_GameField.TabStop = false;
+            this.pictureBox_GameField.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_GameField_Paint);
+            this.pictureBox_GameField.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_GameField_MouseClick);
+            this.pictureBox_GameField.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_GameField_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 386);
+            this.Controls.Add(this.pictureBox_Pause);
+            this.Controls.Add(this.pictureBox_Clear);
+            this.Controls.Add(this.pictureBox_Step);
+            this.Controls.Add(this.pictureBox_Fill);
+            this.Controls.Add(this.pictureBox_Play);
+            this.Controls.Add(this.pictureBox_GameField);
+            this.MaximumSize = new System.Drawing.Size(360, 425);
+            this.MinimumSize = new System.Drawing.Size(360, 425);
+            this.Name = "Form1";
+            this.Text = "Conway\'s Game of Life";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Clear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Step)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Fill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Play)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_GameField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_GameField;
+        private System.Windows.Forms.PictureBox pictureBox_Play;
+        private System.Windows.Forms.PictureBox pictureBox_Fill;
+        private System.Windows.Forms.PictureBox pictureBox_Step;
+        private System.Windows.Forms.PictureBox pictureBox_Clear;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox_Pause;
+    }
+}
+
---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    //--------------------------------------------------------------
+    // Игра «Жизнь» (англ. Conway's Game of Life) — клеточный автомат, 
+    // придуманный английским математиком Джоном Конвеем в 1970 году.
+    //--------------------------------------------------------------
+    //--------------------Правила игры------------------------------
+    // Место действия игры -"вселенная", имеющая размеченная на клетки поверхность.
+    // Каждая клетка может быть "живой" (зеленая) либо "мертвой" (черная)
+    // Игра начинается с нулевого поколения.
+    // Каждый шаг игры - это зарождение нового поколения.
+    // Каждая клетка имеет вокруг себя 8 соседей.
+    // Поколение появляется следующим образом:
+    //      1. в "мертвой" клетке, рядом с которой ровно 3 живые клетки - зарождается жизнь
+    //      2. если у живой клетки есть две или три живые соседки, то эта клетка продолжает жить; 
+    //      3. в противном случае, если соседей меньше двух или больше трёх, клетка умирает 
+    //      («от одиночества» или «от перенаселённости»)
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        static ushort lifeSize = 32; // кол-во жизненных клеток в PictureBox
+        static ushort pointRadius = 10; // радиус одной жизненной клетки
+        Generation gen = new Generation(lifeSize);
+        SolidBrush blackBrush, greenBrush;
+        Rectangle[,] Cells = new Rectangle[lifeSize, lifeSize];
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            pictureBox_GameField.Image = 
+                new Bitmap(pictureBox_GameField.Width, pictureBox_GameField.Height);
+            g = Graphics.FromImage(pictureBox_GameField.Image);
+            blackBrush = new SolidBrush(Color.Black);
+            greenBrush = new SolidBrush(Color.Green);
+            ToolTip t = new ToolTip();
+            t.SetToolTip(pictureBox_Fill, "Сгенировать поколение случайным образом");
+            t.SetToolTip(pictureBox_Step, "Перейти к следующему поколению");
+            t.SetToolTip(pictureBox_Play, "Автоматическая генерация поколений");
+            t.SetToolTip(pictureBox_Clear, "Очистить поле");
+            pictureBox_Play.Enabled = false;
+        }
+
+        private void pictureBox_Play_Click(object sender, EventArgs e)
+        {
+
+            pictureBox_Play.Visible = false;
+            pictureBox_Pause.Visible = true;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+            /// <summary>
+            /// Нарисовать поле
+            /// </summary>
+        private void PaintField()
+        {
+            g = Graphics.FromHwnd(pictureBox_GameField.Handle);
+            g.FillRectangle(blackBrush, 0, 0,
+                pictureBox_GameField.Width, pictureBox_GameField.Height);
+            for (int i = 0; i < lifeSize; i++)
+            {
+                for (int j = 0; j < lifeSize; j++)
+                {
+                    if (gen.getNextGeneration(i, j))
+                    {
+                        g.FillRectangle(greenBrush, i * pointRadius, j * pointRadius,
+                            pointRadius, pointRadius);
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_Clear_Click(object sender, EventArgs e)
+        {
+            g = Graphics.FromHwnd(pictureBox_GameField.Handle);
+            g.FillRectangle(blackBrush, 0, 0,
+                pictureBox_GameField.Width, pictureBox_GameField.Height);
+            gen.Clear();
+        }
+
+        private void pictureBox_Step_Click(object sender, EventArgs e)
+        {
+            gen.NextGeneration();
+            PaintField();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox_Step_Click(pictureBox_Step, null);
+        }
+
+        private void pictureBox_Pause_Click(object sender, EventArgs e)
+        {
+            pictureBox_Play.Visible = true;
+            pictureBox_Pause.Visible = false;
+            timer1.Enabled = false;
+            timer1.Stop();
+        }
+
+        private void pictureBox_GameField_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Black);
+            for (int i = 0; i < lifeSize; i++)
+            {
+                for (int j = 0; j < lifeSize; j++)
+                {
+                    Cells[i, j] = new Rectangle(
+                        i * pictureBox_GameField.Width / (lifeSize),
+                        j * pictureBox_GameField.Height / (lifeSize), pointRadius, pointRadius);
+                    g.FillRectangle(blackBrush, Cells[i, j]);
+                }
+            }
+        }
+
+        private void pictureBox_GameField_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = Graphics.FromHwnd(pictureBox_GameField.Handle);
+            var location = e.Location;
+            int x = location.X;
+            int y = location.Y;
+            for (int i = 0; i < lifeSize; i++)
+            {
+                for (int j = 0; j < lifeSize; j++)
+                {
+                    if (Cells[i, j].Contains(x, y))
+                    {
+                        Cells[i, j] = new Rectangle(
+                        i * pictureBox_GameField.Width / (lifeSize),
+                        j * pictureBox_GameField.Height / (lifeSize), pointRadius, pointRadius);
+                        gen.ChangeColorCell(i, j);
+                        if (gen.getLifeGeneration(i, j))
+                            g.FillRectangle(greenBrush, Cells[i, j]);
+                        else g.FillRectangle(blackBrush, Cells[i, j]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_GameField_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                for (int i = 0; i < lifeSize; i++)
+                {
+                    for (int j = 0; j < lifeSize; j++)
+                    {
+                        if ((e.Y >= (Cells[i, j].Location.Y)) &&
+                        (e.Y <= (Cells[i, j].Location.Y + pointRadius)) &&
+                        (e.X >= Cells[i, j].Location.X) &&
+                        (e.X <= (Cells[i, j].Location.X + pointRadius)))
+                        {
+
+                            gen.ChangeColorCell(i, j);
+                            if (gen.getLifeGeneration(i, j))
+                                g.FillRectangle(greenBrush, Cells[i, j]);
+                            else g.FillRectangle(blackBrush, Cells[i, j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_Fill_Click(object sender, EventArgs e)
+        {
+            pictureBox_Play.Enabled = true;
+            gen.RandomGeneration();
+            PaintField();
+        }
+    }
+}
